0bb3c7d0e39c57d6ed6babc4b2d80c192269ec14
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2023-10-01 18:07:43 -0300

    feat: add card with pokemon names and url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
-const getPokemonFromApi = () => {
-    return fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`)
+const getPokemonFromApi = (endpoint) => {
+    return fetch(`https://pokeapi.co/api/v2/${endpoint}`)
     .then(something => something.json())
     .then(({results}) => results)
     .then(results => results.map((pokemon)=> {
@@ -12,5 +12,35 @@ const getPokemonFromApi = () => {
     
 }
 
-getPokemonFromApi()
-    .then((pokemon) => console.log(pokemon))
+
+
+const createCard = ({name, url}) => `
+            <div class="row container m-1">
+                <div class="col-sm-6 mb-3 mb-sm-0 container">
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${name}</h5>
+                        <p class="card-text">${url}</p>
+                        <a href="#" class="btn btn-primary">Mas info</a>
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+`
+const pageContent = document.getElementById("content");
+
+
+
+
+
+const renderPage = async () => {
+    const firstHundredPokemons = await getPokemonFromApi("pokemon?limit=100&offset=0")
+
+    const template = firstHundredPokemons
+    .map(pokemon => createCard(pokemon))
+    .join("")
+    pageContent.innerHTML = template
+}
+
+renderPage();
